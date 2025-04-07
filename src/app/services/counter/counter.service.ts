373a400b03bee0522a30c2fad6720cb49bd1279e
labstraction <labstraction@protonmail.com>
2025-04-07 10:40:23 +0200

    counter project

---
+++ src/app/services/counter/counter.service.ts
@@ -0,0 +1,57 @@
+import { computed, effect, Injectable, signal } from '@angular/core';
+import { count } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+  ciao = signal('hello')
+
+  counter = signal(0);
+
+  doubleCounter = computed(() => this.counter() * 2)
+
+
+  constructor() {
+
+      effect(() => {
+        console.log('E1 il contatore vale: ', this.counter(), ' e il doppio è: ', this.doubleCounter())
+        
+      })
+
+      effect(()=>{
+        console.log('E2 il contatore vale: ', this.counter(), ' e il doppio è: ', this.doubleCounter())
+        console.log(this.ciao())
+      })
+
+
+      this.ciao.update(str => str + ' world!')
+      
+
+      //this.counter.set(8);
+
+      setTimeout(() => {
+        this.counter.update(numb => numb + 100);
+      }, 1000);
+
+
+      setTimeout(() => {
+        this.ciao.set('pippo')
+      }, 2000);
+
+      
+
+
+      // console.log('il contatore adesso vale: ', this.counter(), ' e il doppio è: ', this.doubleCounter())
+
+
+      //this.counter.update(oldValue => oldValue + 1)
+
+
+      // console.log('il contatore adesso vale: ', this.counter(), ' e il doppio è: ', this.doubleCounter())
+  }
+
+
+
+}
